43932802cd0ae412dfa6084715ab16a79dab6ae4
muhammadraja12 <muhammadrajaindrafata23@gmail.com>
2025-12-18 00:39:20 +0800

    Tambahan Comment untuk memudahkan programer selanjutnya.

--- Program_Cek_Password.go
+++ Program_Cek_Password.go
@@ -3,19 +3,23 @@ package main
 import "fmt"
 
 func main() {
-		var password string
 
+		//Input password dari user
+		var password string
 		fmt.Print("Masukkan password: ")
 		fmt.Scan(&password)
-
+ 
+		// Inisialisasi variabel
 		var hasUpper, hasLower, hasDigit bool 
 		var length, i int 
 		var ch byte
 
+		// Set awal variabel
 		hasUpper = false
 		hasLower = false
 		hasDigit = false
 
+		// Karakter yang ada pada password
 		for i = 0; i < len(password); i++ { 
 			ch = password[i]
 			length++
@@ -29,10 +33,8 @@ func main() {
 			} 
 		}
 
-		
-
-		
-
+			
+		// Requirment penilaian kekuatan password
 		if length < 8 {
 			fmt.Println("Password minimal 8 karakter.")
 		} else if hasUpper && hasLower && hasDigit {
